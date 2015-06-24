3e27e9eb1858aed63d5d68e3c519f4381c932367
Timothy Strimple <tim@tstrimple.com>
2015-06-24 11:57:29 -0500

    added images

--- DiscoveryEducation/DiscoveryEducation.Shared/injection-script-example.js
+++
@@ -1,6 +0,0 @@
-﻿// This script will be executed in the context of the webview
-var foo = function () {
-    console.log("Injection successful");
-};
-
-foo();
---
+++ DiscoveryEducation/DiscoveryEducation.Shared/js/injection-script.js
@@ -0,0 +1,21 @@
+﻿/* 
+   OBROWN : Only for Phones, the buttons for Caldar / List view are in the way.
+   These buttons are moved to the Nar Bar and utilize this function to toggle views
+   code modified from what Discovery Ed Dev team sent over
+*/
+function toggleCalendarView() {
+    $('#assignments').toggle();
+    $('#filters').toggle();
+    $('.btn-toolbar .btn-group').toggle();
+    calendarView = !calendarView;
+    $('#student-assignments-calendar').toggle();
+
+    if (!calendarView) {
+        filterKey.calendarDate = "";
+        $('#student-assignments-calendar').empty();
+        assignmentsQryStr();
+    }
+    else {
+        getCalendar();
+    }
+}
