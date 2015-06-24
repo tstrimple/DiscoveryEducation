d770c08c9a695c0c0bca8bb0a9b383888eeaa7dc
Timothy Strimple <tim@tstrimple.com>
2015-06-24 17:59:08 -0500

    Style tweaks

--- DiscoveryEducation/DiscoveryEducation.Shared/js/injection-script.js
+++ DiscoveryEducation/DiscoveryEducation.Shared/js/injection-script.js
@@ -1,4 +1,23 @@
-﻿
+﻿function updateViewport() {
+    var viewportWidth = $(window).width();
+    var viewportHeight = $(window).height();
+
+    setContainerWidth(viewportWidth);
+    setVideoSize(viewportWidth, viewportHeight);
+}
+
+function setContainerWidth(width) {
+    $('.container').width(width);
+}
+
+function setVideoSize(width, height) {
+   // $('.container').width(400);
+   // $('.container').height(300);
+}
+
+$(document).ready(updateViewport);
+$(window).resize(updateViewport);
+
 function toggleCalendarView() {
     if ($('#assignments').is(':visible')) {
         $('.view-switcher-inner .btn[data-view="calendar"]').click();
@@ -6,4 +25,12 @@ function toggleCalendarView() {
     else {
         $('.view-switcher-inner .btn[data-view="list"]').click();
     }
+}
+
+function showWelcome() {
+    $('a.tabnav.tab1')[0].click();
+}
+
+function showAssignments() {
+    $('a.tabnav.tab2')[0].click();
 }
